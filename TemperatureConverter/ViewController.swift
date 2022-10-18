ffd8f1a53bafef9ac7d624e8efcc7cd7bb113af3
Maciej Hełmecki <maciej.helmecki@accenture.com>
2022-10-18 14:40:23 +0200

    Init project

---
+++ TemperatureConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TemperatureConverter
+//
+//  Created by maciej.helmecki on 18/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
