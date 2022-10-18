438fb96c72ee8ce653d689970103d00d7c518435
Maciej Hełmecki <maciej.helmecki@accenture.com>
2022-10-18 22:42:59 +0200

    Add UI components

--- TemperatureConverter/Scenes/HomeViewController.swift
+++ TemperatureConverter/Scenes/HomeViewController.swift
@@ -11,13 +11,77 @@ import SnapKit
 class HomeViewController: UIViewController {
     private enum Constants {
         static let marginOffset: CGFloat = 24.0
+        static let stackViewSpacing: CGFloat = 32.0
+        static let boxViewSpacing: CGFloat = 8.0
     }
 
-    // MARK: - Properties
+    static let labelConfiguration: (UILabel) -> Void = {
+        $0.textColor = .lightGray
+        $0.font = UIFont.preferredFont(forTextStyle: .caption1)
+    }
+
+    static let boxStackViewConfiguration: (UIStackView) -> Void = {
+        $0.axis = .vertical
+        $0.spacing = Constants.boxViewSpacing
+    }
+
+    static let underlineViewConfiguration: (UIView) -> Void = {
+        $0.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
+        $0.backgroundColor = .systemGray6
+    }
+
+    // MARK: - UI elements
+    let stackView = UIStackView().configure {
+        $0.axis = .vertical
+        $0.spacing = Constants.stackViewSpacing
+    }
+
+    let stackView2 = UIStackView().configure {
+        $0.axis = .horizontal
+        $0.spacing = 16.0
+        $0.distribution = .fillEqually
+    }
+
     let headlineLabel = UILabel().configure {
         $0.text = "Temperature converter"
         $0.textColor = .black
         $0.font = UIFont.preferredFont(forTextStyle: .headline)
+        $0.textAlignment = .center
+    }
+
+    let degreesBoxStackView = UIStackView().configure(HomeViewController.boxStackViewConfiguration)
+    let degreesTitleLabel = UILabel()
+        .configure(HomeViewController.labelConfiguration)
+        .configure { $0.text = "Degrees" }
+    let degreesTextView = UITextView().configure {
+        $0.heightAnchor.constraint(equalToConstant: 32.0).isActive = true
+        $0.font = UIFont.preferredFont(forTextStyle: .body)
+        $0.textColor = .darkGray
+    }
+
+    let typeBoxStackView = UIStackView().configure(HomeViewController.boxStackViewConfiguration)
+    let typeTitleLabel = UILabel()
+        .configure(HomeViewController.labelConfiguration)
+        .configure { $0.text = "Type" }
+    let typeButton = UIButton(type: .system).configure {
+        $0.setTitle("Fahrenheit", for: .normal)
+        $0.setTitleColor(.darkGray, for: .normal)
+        $0.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+        $0.contentHorizontalAlignment = .leading
+    }
+
+    let resultBoxStackView = UIStackView().configure(HomeViewController.boxStackViewConfiguration)
+    let resultTitleLabel = UILabel()
+        .configure(HomeViewController.labelConfiguration)
+        .configure { $0.text = "Result" }
+    let resultLabel = UILabel().configure {
+        $0.text = "22.222"
+        $0.textColor = .darkGray
+        $0.font = UIFont.preferredFont(forTextStyle: .body)
+    }
+
+    let convertButton = UIButton(type: .system).configure {
+        $0.setTitle("Convert", for: .normal)
     }
 
     // MARK: - Lifecycle
@@ -38,14 +102,42 @@ class HomeViewController: UIViewController {
 private extension HomeViewController {
     // MARK: - Methods
     func setupView() {
-        view.backgroundColor = .systemGray6
+        view.backgroundColor = .white
 
-        view.addSubview(headlineLabel)
-        headlineLabel.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(Constants.marginOffset)
-            $0.centerX.equalToSuperview()
+        [degreesTitleLabel, degreesTextView, UIView().configure(HomeViewController.underlineViewConfiguration)]
+            .forEach(degreesBoxStackView.addArrangedSubview)
+        [typeTitleLabel, typeButton, UIView().configure(HomeViewController.underlineViewConfiguration)]
+            .forEach(typeBoxStackView.addArrangedSubview)
+        [resultTitleLabel, resultLabel, UIView().configure(HomeViewController.underlineViewConfiguration)]
+            .forEach(resultBoxStackView.addArrangedSubview)
+
+        [degreesBoxStackView, typeBoxStackView].forEach(stackView2.addArrangedSubview)
+        view.addSubview(stackView)
+        [
+            headlineLabel,
+            stackView2,
+            resultBoxStackView,
+            convertButton
+        ].forEach(stackView.addArrangedSubview)
+
+        stackView.snp.makeConstraints {
+            $0.top.leading.equalTo(view.safeAreaLayoutGuide).offset(Constants.marginOffset)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-Constants.marginOffset)
         }
+//        installConstraints()
     }
+
+//    func installConstraints() {
+//        headlineLabel.snp.makeConstraints {
+//            $0.top.equalTo(view.safeAreaLayoutGuide).offset(Constants.marginOffset)
+//            $0.centerX.equalToSuperview()
+//        }
+//
+//        convertButton.snp.makeConstraints {
+//            $0.centerX.equalToSuperview()
+//            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-Constants.marginOffset)
+//        }
+//    }
 
     func bind() {}
 }
